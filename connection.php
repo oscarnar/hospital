65fd9faf0ecb9c281a12d38afcaa1671d7899082
oscarnar <orosas58@gmail.com>
2019-12-11 11:44:33 -0500

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
-$con = mysqli_connect("localhost","admin","123","Hospital");
+$con = mysqli_connect("b8slhno9ljt14jt3brw1-mysql.services.clever-cloud.com","uuy4zlyrdzste11o","NEKIVATBYCviIvQ2Eohr","b8slhno9ljt14jt3brw1");
 if(!$con){
     echo "Error al conectar con la base de datos.";
 }
